fb44929645f632f043accaf0c2fbf91fe00a380b
sathishkumar.yoho <sathishkumar.yoho@gmail.com>
2026-03-04 18:47:11 +0530

    changes

--- src/routes/Approutes.tsx
+++ src/routes/Approutes.tsx
@@ -1,12 +1,45 @@
 import { Route, Routes } from "react-router-dom";
-import Profile from "../pages/Profile/profile";
-
+import { Mainlayout } from "../layout/MainLayout";
+import ProtectedRoute from "./ProtectedRoute";
+import NotFound from "../components/Shared/NotFound";
+import { PublicRoute } from "./PublicRoute";
+import { SignIn } from "../pages/Auth/Signin";
+import { Dashboard } from "../pages/Dashboard/Dashboard";
+import VehicleManagement from "../pages/VehicleManagement/VehicleManagement";
+import AddVehicle from "../components/VehicleManagement/AddVehicle";
+import FamilyTenants from "../pages/FamilyTenants/FamilyTenants"
+import VisitorManagement from '../pages/VisitorManagement/VisitorManagement';
+import MyProfile from "../pages/Profile/profile";
 
 export default function AppRoutes() {
   return (
     <div>
       <Routes>
-       <Route path="profile" element={<Profile />} />
+        <Route
+          path="/signin"
+          element={
+            <PublicRoute>
+              <SignIn />
+            </PublicRoute>
+          }
+        />
+        <Route
+          element={
+            <ProtectedRoute>
+              <Mainlayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="/vehicles&parking" element={<VehicleManagement />} />
+          <Route path="/family&tenants" element={<FamilyTenants />} />
+          <Route path="/visitors" element={<VisitorManagement />} />
+          <Route path="/my-profile" element={<MyProfile />} />
+          <Route path="/add-vehicle" element={<AddVehicle setOpenModal={function (): void {
+            throw new Error("Function not implemented.");
+          } } />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
