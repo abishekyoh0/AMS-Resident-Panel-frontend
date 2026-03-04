7b7080bb49d8518406aa0cade44b488be42730eb
sathishkumar.yoho <sathishkumar.yoho@gmail.com>
2026-03-04 14:55:26 +0530

    changes in Sidebar

--- src/components/Auth/MoveInForm.tsx
+++ src/components/Auth/MoveInForm.tsx
@@ -2,37 +2,42 @@ export default function MoveInForm() {
 
     return (
 
-        <div className="min-h-screen text-white p-6 flex justify-center">
 
-            <div className="max-w-4xl w-full space-y-6">
+            <div className="max-w-4xl w-full space-y-2">
 
                 <h2 className="text-3xl font-bold">New Move-IN Form</h2>
 
 
-                <div className="p-6 rounded-xl">
+                <div className="bg-[#FFFFFF1A] p-6 rounded-xl">
 
                     <h3 className="text-lg mb-4">Tenant Information</h3>
 
                     <div className="grid md:grid-cols-2 gap-4">
-
-                        <input placeholder="Full Name" className="input" />
-                        <input placeholder="Phone Number" className="input" />
-
+                        <div>
+                            <label htmlFor="">Full name</label>
+                            <input placeholder="Full Name" className="input border" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Phone number</label>
+                            <input placeholder="Phone Number" className="input border" />
+                        </div>
                     </div>
-
-                    <input placeholder="Email Address" className="input mt-4" />
+                    <div>
+                        <label htmlFor="">Email Address</label>
+                        <input placeholder="Email Address" className="input border" />
+                    </div>
 
                 </div>
 
 
-                <div className="p-6 rounded-xl">
+                <div className="bg-[#FFFFFF1A] p-6 rounded-xl">
 
                     <h3 className="mb-4">Property Information</h3>
 
                     <div className="grid md:grid-cols-2 gap-4">
 
-                        <input placeholder="Tower Name" className="input" />
-                        <input placeholder="Tower No" className="input" />
+                        <input placeholder="Tower Name" className="input border" />
+                        <input placeholder="Tower No" className="input border" />
 
                         <select className="input">
                             <option>Select Resident Type</option>
@@ -45,19 +50,19 @@ export default function MoveInForm() {
                 </div>
 
 
-                <div className="p-6 rounded-xl">
+                <div className="bg-[#FFFFFF1A] p-6 rounded-xl">
 
                     <h3 className="mb-4">Security Deposit</h3>
 
-                    <input placeholder="Deposit Amount" className="input mb-4" />
-                    <input placeholder="Maintenance Amount" className="input" />
+                    <input placeholder="Deposit Amount" className="input border mb-4" />
+                    <input placeholder="Maintenance Amount" className="input border" />
 
                 </div>
 
                 <div className="flex justify-end gap-4">
 
                     <button onClick={() => window.history.back()}
-                     className="px-6 py-2 bg-gray-700 rounded-lg">
+                        className="px-6 py-2 bg-gray-700 rounded-lg">
                         Cancel
                     </button>
 
@@ -69,7 +74,6 @@ export default function MoveInForm() {
 
             </div>
 
-        </div>
 
     )
 }
--- src/components/Shared/Sidebar.tsx
+++ src/components/Shared/Sidebar.tsx
@@ -3,31 +3,28 @@ import { COLORS, FONTSIZE, WEIGHT } from "../../constent/uiconstent";
 import { useState } from "react";
 import LogoutModal from "./logout";
 import { useAuth } from "../Auth/AuthContext";
-import dashboardicon from "../../assets/Sidebar/graph.png";
-import accessicon from "../../assets/Sidebar/access.png";
-import buildingicon from "../../assets/Sidebar/building.png";
-import usericon from "../../assets/Sidebar/user.png";
-import complainticon from "../../assets/Sidebar/complaints.png";
-import visitorsicon from "../../assets/Sidebar/visitor.png";
-import invoicesicon from "../../assets/Sidebar/invoice.png";
-import entryicon from "../../assets/Sidebar/entry.png";
-import emergencyicon from "../../assets/Sidebar/emergency.png";
-import logoicon from "../../assets/Sidebar/settings.png";
+import dashboard from "../../assets/Sidebar/graph.png";
+import family from "../../assets/Sidebar/family.png";
+import vehicle from "../../assets/Sidebar/car.png";
+import profile from "../../assets/Sidebar/user.png";
+import complaint from "../../assets/Sidebar/notepencil.png";
+import visitor from "../../assets/Sidebar/hand.png";
+import invoice from "../../assets/Sidebar/currency.png";
+import emergency from "../../assets/Sidebar/alarm.png";
+import logoicon from "../../assets/Sidebar/home.png";
 import logoutIcon from "../../assets/Sidebar/logout.png"
-import parkingicon from "../../assets/Sidebar/parking.png"
+import entry from "../../assets/Sidebar/home.png"
 
 const menuItems = [
-  { to: "/", label: "Dashboard", icon: dashboardicon, end: true },
-  { to: "/access-management", label: "Access Management", icon: accessicon, end: false },
-  { to: "/building", label: "Building", icon: buildingicon, end: true },
-  { to: "/user", label: "Users", icon: usericon, end: true },
-  { to: "/complaint", label: "Complaints", icon: complainticon, end: true },
-  { to: "/visitors", label: "Visitors", icon: visitorsicon, end: true },
-  { to: "/invoices", label: "Invoices", icon: invoicesicon, end: true },
-  { to: "/parking", label: "Parking", icon: parkingicon, end: true },
-  { to: "/emergency-control", label: "Emergency Control", icon: emergencyicon, end: true },
-  { to: "/entry-reports", label: "Entry Reports", icon: entryicon, end: true },
-
+  { to: "/", label: "Dashboard", icon: dashboard, end: true },
+  { to: "/my-profile", label: "My Profile", icon: profile, end: true },
+  { to: "/family & tenants", label: "Family & Tenants", icon: family, end: true },
+  { to: "/vehicles & parking", label: "Vehicles & Parking", icon: vehicle, end: true },
+  { to: "/complaint", label: "My Complaints", icon: complaint, end: true },
+  { to: "/invoices", label: "Invoices", icon: invoice, end: true },
+  { to: "/visitors", label: "Visitors", icon: visitor, end: true },
+  { to: "/entry-history", label: "Entry History", icon: entry, end: true },
+  { to: "/emergency-alert", label: "Emergency Alerts", icon: emergency, end: true },
 ];
 
 type SidebarProps = {
@@ -52,13 +49,13 @@ export const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
         className={`fixed md:static  z-50 top-0 h-full left-0 w-60 p-4 bg-[#000000] transform transition-transform duration-300 overflow-x-auto no-scrollbar ${isOpen ? "translate-x-0" : "-translate-x-full"} md:translate-x-0`}
         style={{ color: COLORS.primary_white }}
       >
-        <div className="flex flex-col items-center gap-2 mb-4 px-5 py-3 rounded-xl bg-linear-to-r from-[#00B8DB] to-[#007595] ">
+        <div className="flex flex-col items-center gap-2 mb-4 px-5 py-3 rounded-xl bg-linear-to-r from-[#2B7FFF] to-[#1447E6] ">
           <img src={logoicon} alt="Logo" className="w-15 h-15" />
           <h1
             style={{ color: COLORS.primary_white, fontWeight: WEIGHT.seven }}
             className={`${FONTSIZE[20]}`}
           >
-            Admin Panel
+            Resident Panel
           </h1>
         </div>
         <div className="flex flex-col justify-between">
@@ -91,7 +88,7 @@ export const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
             <div className="mt-7 ">
               <button
                 onClick={() => setShowLogoutModal(true)}
-                className="w-full flex items-center gap-3 px-4 py-2 rounded-lg bg-[linear-gradient(90deg,rgba(0,184,219,0.2)_0%,rgba(142,81,255,0.2)_100%)] text-white  transition cursor-pointer"
+                className="w-full flex items-center gap-3 px-4 py-2 rounded-lg bg-[#FF0004] text-white  transition cursor-pointer"
               >
                 <img src={logoutIcon} alt="Logout" className="w-5 h-5" />
                 <div className="flex justify-center  w-full">
